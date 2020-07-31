523ea35b965a9fdf85fa7a7124b9466ad9559c89
Ben Johnson <bkj.322@gmail.com>
2020-07-31 10:46:28 -0400

    docs

--- external_function/run.sh
+++ external_function/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# --
+# Build
+
 rm -f main
 rm -f main.cpp
 
 GRAPHIT_ROOT=/home/bjohnson/software/graphit
-
 
 python $GRAPHIT_ROOT/build/bin/graphitc.py \
     -f "./main.gt"                         \
@@ -12,3 +14,8 @@ python $GRAPHIT_ROOT/build/bin/graphitc.py \
     -o main.cpp
 
 g++ -std=c++14 -I "$GRAPHIT_ROOT/src/runtime_lib/" -O3 -o main main.cpp external.cpp
+
+# --
+# Run
+
+./main
